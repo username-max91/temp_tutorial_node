579513504c749f9962b4cc35b3f58d7abe1bf916
username-max91 <70011815+username-max91@users.noreply.github.com>
2023-08-04 23:42:27 +0200

    moved file from a wrong folder

---
+++ 14-setTimeout.js
@@ -0,0 +1,10 @@
+const { setTimeout } = require("timers/promises")
+
+//started OS process
+console.log('first')
+setTimeout(() => {
+    console.log('second')
+}, 0)
+
+console.log('third')
+//completed and exited OS process
