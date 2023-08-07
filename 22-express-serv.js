195964d75c8a9c32ca6290551b2ebde5a75ddaff
username-max91 <70011815+username-max91@users.noreply.github.com>
2023-08-07 12:17:56 +0200

    course updates commit

---
+++ 22-express-serv.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+
+app.get('/',(req,res)=>{
+    res.status(200).send('Home page')
+})
+app.get('/about',(req,res)=>{
+    res.status(200).send('About page')
+})
+app.all('*',(req,res)=>{
+    res.status(404).send('<h1>resource not found</h1>')
+})
+
+app.listen(5000,()=>{
+    console.log('server is on port 5000')
+})
